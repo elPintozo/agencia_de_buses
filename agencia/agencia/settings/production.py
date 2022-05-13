4aaeabc96a5fa530816a6f629380eec90bf93700
RicardoR <ricardo.sr.lepe@gmail.com>
2022-05-13 19:51:03 -0400

    primera subida, base del proyecto

---
+++ agencia/agencia/settings/production.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', False)
+
+ALLOWED_HOSTS = []
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = 'static/'
